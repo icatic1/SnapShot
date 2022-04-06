0688b16a48b69252d188aa3b6b0548e7836b8e41
Irvin Catic <icatic1@etf.unsa.ba>
2022-04-06 12:06:50 +0200

    Add Model and View folders

---
+++ SnapShot/SnapShot/Model/Configuration.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SnapShot
+{
+    public class Configuration
+    {
+        #region Attributes
+
+        // device configuration
+        DeviceType type;
+        string id = "";
+        string triggerFilePath = "",
+               outputFolderPath = "";
+        int outputValidity;
+
+        // video configuration
+        Resolution resolution;
+        int contrastLevel;
+        Color imageColor;
+        bool motionDetection = false;
+
+        // network configuration - not a part of this sprint
+        string serverVersion = "",
+               serverIP = "";
+        int serverPort;
+        bool connectionStatus = false;
+
+        // capture configuration
+        bool imageCapture = true,
+             singleMode = true;
+        int duration,
+            period;
+
+        #endregion
+
+        #region Properties
+
+        public DeviceType Type { get => type; set => type = value; }
+        public string Id { get => id; set => id = value; }
+        public string TriggerFilePath { get => triggerFilePath; set => triggerFilePath = value; }
+        public string OutputFolderPath { get => outputFolderPath; set => outputFolderPath = value; }
+        public int OutputValidity { get => outputValidity; set => outputValidity = value; }
+        public Resolution Resolution { get => resolution; set => resolution = value; }
+        public int ContrastLevel { get => contrastLevel; set => contrastLevel = value; }
+        public Color ImageColor { get => imageColor; set => imageColor = value; }
+        public bool MotionDetection { get => motionDetection; set => motionDetection = value; }
+        public string ServerVersion { get => serverVersion; set => serverVersion = value; }
+        public string ServerIP { get => serverIP; set => serverIP = value; }
+        public int ServerPort { get => serverPort; set => serverPort = value; }
+        public bool ConnectionStatus { get => connectionStatus; set => connectionStatus = value; }
+        public bool ImageCapture { get => imageCapture; set => imageCapture = value; }
+        public bool SingleMode { get => singleMode; set => singleMode = value; }
+        public int Duration { get => duration; set => duration = value; }
+        public int Period { get => period; set => period = value; }
+
+        #endregion
+
+        #region Constructor
+
+        public Configuration() { }
+
+        #endregion
+
+        #region Methods
+
+        // not a part of this string
+        public bool CheckServerConnectionStatus()
+        {
+            return false;
+        }
+
+        #endregion
+    }
+}
---
+++ SnapShot/SnapShot/Model/Resolution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapShot
+{
+    public enum Resolution
+    {
+        Resolution720x480,
+        Resolution1280x960,
+        Resolution1280x1024,
+        Resolution1920x1080,
+        Resolution2048x1536
+    }
+}
