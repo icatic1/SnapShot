595403f2f51308e9b0f14960f402707de7521799
Armin <72748909+ahadzic7@users.noreply.github.com>
2022-04-06 23:37:12 +0200

    Create Models Testing project

---
+++ SnapShot/Model Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SnapShot;
+
+namespace Model_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var x = new Snapshot();
+        }
+    }
+}
