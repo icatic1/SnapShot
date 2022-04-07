f5cf7b080864ff621a69dda815b0423061747aff
Hamza <hbegic1@etf.unsa.ba>
2022-04-07 23:52:58 +0200

    Add 10 unit tests in ConfigurationTest class

--- SnapShot/Model Tests/ConfigurationTest.cs
+++ SnapShot/Model Tests/ConfigurationTest.cs
@@ -113,7 +113,86 @@ namespace Model_Tests
 
         }
 
+        [TestMethod]
+        public void TestSetType()
+        {
+            Configuration configuration = new Configuration();
+            configuration.Type = new DeviceType();
+            configuration.Type = DeviceType.USBCamera;
+            Assert.AreNotEqual(configuration.Type.ToString(), "IPCamera");
+            Assert.AreEqual(configuration.Type.ToString(), "USBCamera");
+        }
 
+        [TestMethod]
+        public void TestGetType()
+        {
+            Configuration configuration = new Configuration();
+            Assert.IsNotNull(configuration.Type.ToString());
+        }
+
+        [TestMethod]
+        public void TestSetId()
+        {
+            Configuration configuration = new Configuration();
+            configuration.Id = "noviId";
+            Assert.AreEqual("noviId",configuration.Id);
+        }
+
+        [TestMethod]
+        public void TestGetId()
+        {
+            Configuration configuration = new Configuration();
+            Assert.AreEqual("",configuration.Id);
+        }
+
+
+
+        [TestMethod]
+        public void TestSetTriggerFilePath()
+        {
+            Configuration configuration = new Configuration();
+            configuration.TriggerFilePath = "noviTriggerFilePath";
+            Assert.AreEqual(configuration.TriggerFilePath, "noviTriggerFilePath");
+        }
+
+        [TestMethod]
+        public void TestGetTriggerFilePath()
+        {
+            Configuration configuration = new Configuration();
+            Assert.AreEqual("",configuration.TriggerFilePath);
+        }
+
+
+        [TestMethod]
+        public void TestSetOutputFolderPath()
+        {
+            Configuration configuration = new Configuration();
+            configuration.OutputFolderPath = "noviOutputFolderPath";
+            Assert.AreEqual(configuration.OutputFolderPath, "noviOutputFolderPath");
+        }
+
+        [TestMethod]
+        public void TestGetOutputFolderPath()
+        {
+            Configuration configuration = new Configuration();
+            Assert.AreEqual( "", configuration.OutputFolderPath);
+        }
+
+
+        [TestMethod]
+        public void TestSetOutputValidity()
+        {
+            Configuration configuration = new Configuration();
+            configuration.OutputValidity = 1;
+            Assert.AreEqual(configuration.OutputValidity, 1);
+        }
+
+        [TestMethod]
+        public void TestGetOutputValidity()
+        {
+            Configuration configuration = new Configuration();
+            Assert.AreEqual(0, configuration.OutputValidity);
+        }
 
     }
 }
