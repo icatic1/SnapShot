d5ef5b0741443bec3dc9a0f1f203d262661cc9b1
Armin <72748909+ahadzic7@users.noreply.github.com>
2022-04-14 21:26:54 +0200

    Add TearDown method in TestSession

---
+++ SnapShot/GUI Tests/TestSession.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Tests
+{
+    public class TestSession
+    {
+
+        protected static WindowsDriver<WindowsElement> session;
+        public static WindowsDriver<WindowsElement> desktopSession;
+        public static void TearDown()
+        {
+            if (session != null)
+            {
+                session.Quit();
+                session = null;
+            }
+
+            if (desktopSession != null)
+            {
+                desktopSession.Quit();
+                desktopSession = null;
+            }
+        }
+    }
+}
