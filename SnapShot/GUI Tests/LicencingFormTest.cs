e55efa3b6e18a5b339bacfd402219dcdaa7181b9
Armin <72748909+ahadzic7@users.noreply.github.com>
2022-04-14 21:51:05 +0200

    Create LicencingFormTest

---
+++ SnapShot/GUI Tests/LicencingFormTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace GUI_Tests
+{
+    [TestClass]
+    public class LicencingFormTest : TestSession
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+        [TestMethod]
+        public void Test1()
+        {
+
+        }
+    }
+}
