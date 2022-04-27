6c8b0ce1d7e2ad08a1504f6f292418b23cac1eac
ipjanic1 <ipjanic1@etf.unsa.ba>
2022-04-27 18:16:30 +0200

    Add device and video configuration test

---
+++ SnapShot/GUI Tests/DeviceConfigurationTest.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows;
+using System;
+using Xamarin.Essentials;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium.Remote;
+using System.Threading;
+
+namespace GUI_Tests
+{
+    [TestClass]
+    public class DeviceConfigurationTest : TestSession
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+            var configuration = session.FindElementByName("Configuration");
+            configuration.Click();
+            Thread.Sleep(10000);
+            /*var textBox2 = desktopSession.FindElementByAccessibilityId("textBox2");
+            textBox2.SendKeys(Environment.MachineName.ToString());*/
+
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+
+            /* var textBox2 = desktopSession.FindElementByAccessibilityId("textBox2");
+             textBox2.SendKeys(Environment.MachineName.ToString());
+
+             var closeButton = desktopSession.FindElementByAccessibilityId("Close");
+             closeButton.Click();*/
+
+            TearDown();
+        }
+
+
+        [TestMethod]
+        public void Test1_SelectCamera()
+        {
+            var comboBox2 = desktopSession.FindElementByAccessibilityId("comboBox2");
+            Console.WriteLine(comboBox2.Selected.ToString());
+            Assert.AreEqual("USB camera", comboBox2.Text);
+
+            WindowsElement dropDownSupplier = (WindowsElement)desktopSession.FindElementByAccessibilityId("comboBox2");
+            dropDownSupplier.Click(); dropDownSupplier.SendKeys("IP camera"); dropDownSupplier.Click();
+            Assert.AreEqual("IP camera", comboBox2.Text);
+
+        }
+
+        [TestMethod]
+        public void Test2_USBCameraConnected()
+        {
+            WindowsElement dropDownSupplier = (WindowsElement)desktopSession.FindElementByAccessibilityId("comboBox2");
+            dropDownSupplier.Click(); dropDownSupplier.SendKeys("USB camera"); dropDownSupplier.Click();
+
+
+            WindowsElement comboBoxElement = (WindowsElement)desktopSession.FindElementByAccessibilityId("comboBox1");
+            comboBoxElement.Click();
+            comboBoxElement.SendKeys(Keys.Down);
+            comboBoxElement.SendKeys(Keys.Enter);
+            Assert.IsNotNull(comboBoxElement.Selected);
+            Assert.AreNotEqual(comboBoxElement.Text, "");
+
+        }
+
+        [TestMethod]
+        public void Test3_TriggerFilePath()
+        {
+            WindowsElement textBox1 = (WindowsElement)desktopSession.FindElementByAccessibilityId("textBox1");
+            textBox1.Clear();
+            textBox1.SendKeys("C:\\SnapShotTestingGround\\trigger.txt");
+
+            Assert.AreEqual("C:\\SnapShotTestingGround\\trigger.txt", textBox1.Text);
+
+
+        }
+
+        [TestMethod]
+        public void Test4_TriggerRegex()
+        {
+
+            WindowsElement textBox6 = (WindowsElement)desktopSession.FindElementByAccessibilityId("textBox6");
+            textBox6.Clear();
+            textBox6.SendKeys("\\b(record|capture)\\b");
+
+            Assert.AreEqual("\\b(record|capture)\\b", textBox6.Text);
+
+        }
+
+        [TestMethod]
+        public void Test5_SaveOutput()
+        {
+            WindowsElement textBox2 = (WindowsElement)desktopSession.FindElementByAccessibilityId("textBox2");
+            textBox2.Clear();
+            textBox2.SendKeys("C:\\SnapShotTestingGround\\captures");
+
+            Assert.AreEqual("C:\\SnapShotTestingGround\\captures", textBox2.Text);
+
+
+        }
+
+        //[TestMethod]
+        //public void Test6_KeepCaptureDuration()
+        // {
+        //     WindowsElement textBox12 = (WindowsElement)desktopSession.FindElementByName("Keep capture for:");
+        //    textBox12.Clear();
+        //     textBox12.SendKeys("7");
+        //
+        //     Assert.AreEqual("7", textBox12.Text);
+        // }
+
+
+
+
+    }
+}
---
+++ SnapShot/GUI Tests/VideoConfigurationTest.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows;
+using System;
+using Xamarin.Essentials;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium.Remote;
+using System.Threading;
+
+namespace GUI_Tests
+{
+    [TestClass]
+    public class VideoConfigurationTest : TestSession
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+            Thread.Sleep(10000);
+            var configuration = session.FindElementByName("Configuration");
+            configuration.Click();
+            Thread.Sleep(10000);
+            /*var textBox2 = desktopSession.FindElementByAccessibilityId("textBox2");
+            textBox2.SendKeys(Environment.MachineName.ToString());*/
+
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+
+            /* var textBox2 = desktopSession.FindElementByAccessibilityId("textBox2");
+             textBox2.SendKeys(Environment.MachineName.ToString());
+
+             var closeButton = desktopSession.FindElementByAccessibilityId("Close");
+             closeButton.Click();*/
+
+            TearDown();
+        }
+
+
+        [TestMethod]
+        public void Test1_PickResolution1()
+        {
+
+
+            WindowsElement comboBoxElement = (WindowsElement)desktopSession.FindElementByAccessibilityId("comboBox3");
+            Assert.IsNotNull(comboBoxElement.Selected);
+            Assert.AreEqual(comboBoxElement.Text, "640x480");
+
+        }
+
+        [TestMethod]
+        public void Test2_PickResolution2()
+        {
+            WindowsElement comboBoxElement = (WindowsElement)desktopSession.FindElementByAccessibilityId("comboBox3");
+            comboBoxElement.Click();
+            comboBoxElement.SendKeys(Keys.Down);
+            comboBoxElement.SendKeys(Keys.Down);
+            comboBoxElement.SendKeys(Keys.Down);
+            comboBoxElement.SendKeys(Keys.Enter);
+            Assert.IsNotNull(comboBoxElement.Selected);
+            Assert.AreEqual(comboBoxElement.Text, "1280x1024");
+
+        }
+
+        [TestMethod]
+        public void Test3_Slider1()
+        {
+            WindowsElement slider = (WindowsElement)desktopSession.FindElementByAccessibilityId("trackBar1");
+
+
+
+            Assert.AreEqual("0", slider.Text);
+
+
+        }
+
+        [TestMethod]
+        public void Test4_Slider2()
+        {
+
+            WindowsElement slider = (WindowsElement)desktopSession.FindElementByAccessibilityId("trackBar1");
+            slider.SendKeys(Keys.Right);
+            slider.SendKeys(Keys.Right);
+            slider.SendKeys(Keys.Right);
+
+            Assert.AreEqual("30", slider.Text);
+
+        }
+
+        [TestMethod]
+        public void Test5_Slider3()
+        {
+            WindowsElement slider = (WindowsElement)desktopSession.FindElementByAccessibilityId("trackBar1");
+            slider.SendKeys(Keys.Right);
+            slider.SendKeys(Keys.Right);
+            slider.SendKeys(Keys.Right);
+            slider.SendKeys(Keys.Right);
+            slider.SendKeys(Keys.Right);
+            slider.SendKeys(Keys.Right);
+            slider.SendKeys(Keys.Right);
+            slider.SendKeys(Keys.Right);
+            slider.SendKeys(Keys.Right);
+            slider.SendKeys(Keys.Right);
+            Assert.AreEqual("100", slider.Text);
+
+
+        }
+
+        [TestMethod]
+        public void Test6_MotionDetection()
+        {
+            WindowsElement checkBox = (WindowsElement)desktopSession.FindElementByAccessibilityId("checkBox1");
+            checkBox.Clear();
+            checkBox.Click();
+            Assert.IsTrue(checkBox.Selected);
+            checkBox.Click();
+            Assert.IsFalse(checkBox.Selected);
+        }
+
+        [TestMethod]
+        public void Test7_CameraPreview()
+        {
+            WindowsElement button = (WindowsElement)desktopSession.FindElementByAccessibilityId("button5");
+            button.Click();
+            Thread.Sleep(1000);
+
+            WindowsElement label = (WindowsElement)desktopSession.FindElementByAccessibilityId("label3");
+            Assert.AreEqual("Not recording", label.Text);
+
+        }
+
+
+
+
+
+
+    }
+}
